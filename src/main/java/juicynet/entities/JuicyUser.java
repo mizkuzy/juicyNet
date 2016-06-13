fac93d6ae432a5745553df93c6855a9555d56733
mizkuzy <mizkuzy@yandex.ru>
2016-06-13 17:27:42 +0300

    logging

--- src/main/java/juicynet/entities/JuicyUser.java
+++ src/main/java/juicynet/entities/JuicyUser.java
@@ -11,51 +11,32 @@ import java.util.Set;
 
 /*@Entity
 @Table(name = "USERS")*/
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
-public class JuicyUser implements Serializable {
+public @Data class JuicyUser implements Serializable {
 
     /*@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "HAPPY_USER_ID")*/
-    @Getter
-    @Setter
     private Long id;
 
     /*@Column(name = "NAME")*/
-    @Getter
-    @Setter
     private String name;
 
     /*@Column(name = "SURNAME")*/
-    @Getter
-    @Setter
     private String surname;
 
     /*@Column(name = "LOGIN")*/
-    @Getter
-    @Setter
     private String login;
 
     /*@Column(name = "EMAIL")*/
-    @Getter
-    @Setter
     private String email;
 
     /*@Column(name = "CITY")*/
-    @Getter
-    @Setter
     private String city;
 
     /*@Column(name = "ABOUT_USER")*/
-    @Getter
-    @Setter
     private String aboutUser;
 
     //@Column(name = "AVATAR")
-    @Getter
-    @Setter
     private String avatar;
 
     //@JoinColumn(name = "FRIENDS")//todo может join columns?
@@ -65,13 +46,9 @@ public class JuicyUser implements Serializable {
             inverseJoinColumns = {@JoinColumn(name = "HAPPY_USER_ID")}
     )*/
     //@ForeignKey(name = "FK_SNT_FRIENDS", inverseName = "")
-    @Getter
-    @Setter
     private Set<JuicyUser> friends = new HashSet<JuicyUser>();
 
     /*@JoinColumn(name = "MESSAGES")
     @OneToMany*/
-    @Getter
-    @Setter
     private Set<PrivateMessage> messages = new HashSet<PrivateMessage>();//todo when this list should be initialized?
 }
--- src/test/java/TestLogger.java
+++ src/test/java/TestLogger.java
@@ -1,3 +1,5 @@
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -6,10 +8,17 @@ import org.slf4j.LoggerFactory;
 
 @Log
 public class TestLogger {
+    static Logger logger = LoggerFactory.getLogger(TestLogger.class);
     public static void main(String[] args) {
         //TODO аннотация лог не работает(
-        Logger logger = LoggerFactory.getLogger(TestLogger.class);
+//todo как записывать разные уровни в разные файлы?
         logger.debug("Hello world.");
+        logger.info("info logger");
+        logger.trace("trace logger");
+
+        /*LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        // print logback's internal status
+        StatusPrinter.print(lc);*/
     }
 
 }
