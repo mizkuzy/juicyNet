b244dbb28456db85083296dea04bfb5f473d684d
mizkuzy <mizkuzy@yandex.ru>
2016-06-14 19:53:56 +0300

    web configurations

---
+++ src/main/java/juicynet/servlets/LoginServlet.java
@@ -0,0 +1,8 @@
+package juicynet.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "login", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet{
+}
